10e2596358e8f9440760e1f99277ed7c810cccfb
cooperjane549-cmd <cooperjane549@gmail.com>
2026-04-20 17:10:07 +0300

    Update MainActivity.kt

--- app/src/main/java/co/ke/magictortoise/MainActivity.kt
+++ app/src/main/java/co/ke/magictortoise/MainActivity.kt
@@ -1,24 +1,40 @@
 package co.ke.magictortoise
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.google.android.gms.ads.MobileAds
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.unity3d.ads.IUnityAdsInitializationListener
+import com.unity3d.ads.UnityAds
 
 class MainActivity : AppCompatActivity() {
+
+    // Unity Game ID
+    private val unityGameID = "6094869"
+    private val testMode = false // Set to true if you are still testing
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_MagicTortoise) 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Initialize Ads
+        // Initialize Google Ads
         MobileAds.initialize(this) {}
+
+        // Initialize Unity Ads
+        UnityAds.initialize(applicationContext, unityGameID, testMode, object : IUnityAdsInitializationListener {
+            override fun onInitializationComplete() {
+                Log.d("UNITY_ADS", "Unity Initialization Complete")
+            }
+            override fun onInitializationFailed(error: UnityAds.UnityAdsInitializationError?, message: String?) {
+                Log.e("UNITY_ADS", "Unity Initialization Failed: $message")
+            }
+        })
 
         val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
 
-        // Set default screen
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.nav_host_fragment, DashboardFragment())
