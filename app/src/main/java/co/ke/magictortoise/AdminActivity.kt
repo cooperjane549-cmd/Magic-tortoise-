4dd47d284711d9ca7b018ccf64d5818917533685
cooperjane549-cmd <cooperjane549@gmail.com>
2026-05-03 22:00:04 +0300

    Create AdminActivity.kt

---
+++ app/src/main/java/co/ke/magictortoise/AdminActivity.kt
@@ -0,0 +1,14 @@
+package co.ke.magictortoise
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import co.ke.magictortoise.R
+
+class AdminActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin)
+        
+        // Soon, we will add code here to show Swaps and TikTok proofs
+    }
+}
