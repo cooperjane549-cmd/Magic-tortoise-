ee5297e9ef8a9b6eec0a6a3fb3ba5da6e63951bc
cooperjane549-cmd <cooperjane549@gmail.com>
2026-05-04 18:55:11 +0300

    Update AdminRequest.kt

--- app/src/main/java/co/ke/magictortoise/AdminRequest.kt
+++ app/src/main/java/co/ke/magictortoise/AdminRequest.kt
@@ -1,29 +1,12 @@
 package co.ke.magictortoise
 
-import com.google.firebase.database.IgnoreExtraProperties
-import com.google.firebase.database.PropertyName
-
-@IgnoreExtraProperties
 data class AdminRequest(
     var id: String = "",
-    
-    @get:PropertyName("socialLink")
-    @set:PropertyName("socialLink")
-    var socialLink: String = "",
-    
-    @get:PropertyName("mpesaCode")
-    @set:PropertyName("mpesaCode")
-    var mpesaCode: String = "",
-    
-    @get:PropertyName("status")
-    @set:PropertyName("status")
-    var status: String = "",
-    
-    @get:PropertyName("type")
-    @set:PropertyName("type")
+    val userId: String = "",
     var type: String = "",
-    
-    @get:PropertyName("userId")
-    @set:PropertyName("userId")
-    var userId: String = ""
+    var nodeSource: String = "",        // This fixes the AdminActivity errors
+    val mpesaCode: String = "",
+    val socialLink: String = "",
+    val screenshotBase64: String = "",  // This fixes the AdminAdapter errors
+    val status: String = "pending"
 )
