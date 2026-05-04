5a0a4eaf7cd578eb31d521fd1d530510c8571393
cooperjane549-cmd <cooperjane549@gmail.com>
2026-05-04 20:32:32 +0300

    Update AdminRequest.kt

--- app/src/main/java/co/ke/magictortoise/AdminRequest.kt
+++ app/src/main/java/co/ke/magictortoise/AdminRequest.kt
@@ -1,12 +1,12 @@
 package co.ke.magictortoise
 
 data class AdminRequest(
-    var id: String = "",
-    val userId: String = "",
-    var type: String = "",
-    var nodeSource: String = "",        // This fixes the AdminActivity errors
-    val mpesaCode: String = "",
-    val socialLink: String = "",
-    val screenshotBase64: String = "",  // This fixes the AdminAdapter errors
-    val status: String = "pending"
+    var id: String = "",                // Changed to var so we can set child.key
+    var userId: String = "",            // Changed to var for safety
+    var type: String = "",              // Changed to var so we can set the node name
+    var nodeSource: String = "",        
+    var mpesaCode: String = "",         // Changed to var
+    var socialLink: String = "",        // Changed to var
+    var screenshotBase64: String = "",  // Changed to var
+    var status: String = "pending"      // Changed to var
 )
